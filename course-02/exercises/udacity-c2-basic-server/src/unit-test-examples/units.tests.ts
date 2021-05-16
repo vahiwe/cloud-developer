3543a1f8ec70901a85eb96274bd41818eb1a66db
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2021-05-16 09:09:55 +0100

    Complete unit tests for basic server exercise

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,20 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+describe('concat', () => {
+
+  it('should concatenate the strings', () => {
+    const result = concat("Hello ","Sam");
+    expect(result).to.equal("Hello Sam");
+  });
+
+  it('should concatenate the strings', () => {
+    const result = concat("Hello ","John");
+    expect(result).to.equal("Hello John");
+  });
+
+  it('should throw an error if empty string is passed as parameter', () => {
+    expect(()=>{ concat("","John") }).to.throw('Empty String')
+  });
+
+});
